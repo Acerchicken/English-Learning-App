8edc4fd455ab32b73e9d2ddc0ef860271b2513ba
Đỗ Trí Dũng <22025501@vnu.edu.vn>
2023-11-18 12:11:12 +0700

    [18/11/2023] update game handle event

--- New_Project_Architecture/Dictionary/src/main/java/com/example/dictionary/Controllers/GameController.java
+++ New_Project_Architecture/Dictionary/src/main/java/com/example/dictionary/Controllers/GameController.java
@@ -6,6 +6,7 @@ import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
@@ -19,15 +20,19 @@ public class GameController extends Game implements Initializable {
     @FXML
     private AnchorPane mainGamePane;
     private Scene scene;
-    private Button[] boardButtons;
+    private Button[][] boardButtons;
 
-    private final int GRIDSIZE = 20;
+    private final int GRIDSIZE = 14;
     private final int SIZE = 480;
 
     private final int GAP = 1;
 
     private ArrayList<String> wordList;
     private char[][] gameBoard;
+
+    private int firstPointX;
+    private int firstPointY;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -36,24 +41,93 @@ public class GameController extends Game implements Initializable {
         gameBoard = game.createGameBoard(wordList, GRIDSIZE);
         createBoard(SIZE);
     }
+
     public void createBoard(int size) {
         boardPane = new GridPane();
         boardPane.setMinSize(size, size);
         boardPane.setMaxSize(size, size);
         boardPane.setLayoutX(900 - SIZE);
-        boardPane.setLayoutY((double) (600 - SIZE) /2);
-        boardButtons = new Button[GRIDSIZE*GRIDSIZE];
-        for (int i = 0; i < GRIDSIZE*GRIDSIZE; i++) {
-            boardButtons[i] = new Button("Button #" + (i + 1));
-            boardButtons[i].setMinSize((double) size /GRIDSIZE, (double) size /GRIDSIZE);
-            boardButtons[i].setFont(new Font(8));
-        }
-        int buttonIndex = 0;
+        boardPane.setLayoutY((double) (600 - SIZE) / 2);
+        boardButtons = new Button[GRIDSIZE][GRIDSIZE];
         for (int i = 0; i < GRIDSIZE; i++) {
             for (int j = 0; j < GRIDSIZE; j++) {
-                boardButtons[buttonIndex].setText(String.valueOf(gameBoard[i][j]));
-                boardPane.add(boardButtons[buttonIndex], j, i);
-                buttonIndex++;
+                boardButtons[j][i] = new Button("Button #" + (j + i * GRIDSIZE + 1));
+                boardButtons[j][i].setMinSize((double) size / GRIDSIZE, (double) size / GRIDSIZE);
+                boardButtons[j][i].setFont(new Font(8));
+                boardButtons[j][i].setStyle("-fx-background-color: yellow");
+                boardPane.setGridLinesVisible(false);
+                int finalI = i;
+                int finalJ = j;
+                boardButtons[j][i].setOnMouseEntered(event -> {
+                    int rowIndex = GridPane.getRowIndex(boardButtons[finalJ][finalI]);
+                    int columnIndex = GridPane.getColumnIndex(boardButtons[finalJ][finalI]);
+                    if (rowIndex == firstPointX) {
+                        if (firstPointY <= columnIndex) {
+                            for (int k = firstPointY; k <= columnIndex; k++) {
+                                boardButtons[k][rowIndex].setStyle("-fx-background-color: green;");
+                            }
+                        } else {
+                            for (int k = firstPointY; k >= columnIndex; k--) {
+                                boardButtons[k][rowIndex].setStyle("-fx-background-color: green;");
+                            }
+                        }
+                    } else if (columnIndex == firstPointY) {
+                        if (firstPointX <= rowIndex) {
+                            for (int k = firstPointX; k <= rowIndex; k++) {
+                                boardButtons[columnIndex][k].setStyle("-fx-background-color: green;");
+                            }
+                        } else {
+                            for (int k = firstPointX; k >= rowIndex; k--) {
+                                boardButtons[columnIndex][k].setStyle("-fx-background-color: green;");
+                            }
+                        }
+                    } else if (Math.abs(rowIndex - firstPointX) == Math.abs(columnIndex - firstPointY)) {
+                        if (firstPointX <= rowIndex) {
+                            if (firstPointY <= columnIndex) {
+                                for (int k = 0; k <= rowIndex - firstPointX; k++) {
+                                    boardButtons[firstPointY + k][firstPointX + k].setStyle("-fx-background-color: green;");
+                                }
+                            } else {
+                                for (int k = 0; k <= rowIndex - firstPointX; k++) {
+                                    boardButtons[firstPointY - k][firstPointX + k].setStyle("-fx-background-color: green;");
+                                }
+                            }
+                        } else {
+                            if (firstPointY <= columnIndex) {
+                                for (int k = firstPointX - rowIndex; k >= 0; k--) {
+                                    boardButtons[firstPointY + k][firstPointX - k].setStyle("-fx-background-color: green;");
+                                }
+                            } else {
+                                for (int k = firstPointX - rowIndex; k >= 0; k--) {
+                                    boardButtons[firstPointY - k][firstPointX - k].setStyle("-fx-background-color: green;");
+                                }
+                            }
+                        }
+                    } else {
+                        for (int k = 0; k < GRIDSIZE; k++) {
+                            for (int l = 0; l < GRIDSIZE; l++) {
+                                boardButtons[l][k].setStyle("-fx-background-color: yellow;");
+                            }
+                        }
+                        boardButtons[firstPointY][firstPointX].setStyle("-fx-background-color: green;");
+                    }
+                });
+                boardButtons[j][i].setOnMouseExited(event -> {
+                    boardButtons[finalJ][finalI].setStyle("-fx-background-color: yellow;");
+                });
+                boardButtons[j][i].setOnMouseClicked(event -> {
+                    firstPointX = GridPane.getRowIndex(boardButtons[finalJ][finalI]);
+                    firstPointY = GridPane.getColumnIndex(boardButtons[finalJ][finalI]);
+                    boardButtons[finalJ][finalI].setStyle("-fx-background-color: green;");
+                });
+            }
+        }
+
+        for (int i = 0; i < GRIDSIZE; i++) {
+            for (int j = 0; j < GRIDSIZE; j++) {
+                boardButtons[j][i].setText(String.valueOf(gameBoard[i][j]));
+                boardPane.add(boardButtons[j][i], j, i);
+
             }
         }
         mainGamePane.getChildren().add(boardPane);
